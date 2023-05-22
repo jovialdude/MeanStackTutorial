c5afcc4e7318fd7cbd2bc6c8bfdf15fb280b586b
jovialdude <datn11617@gmail.com>
2023-05-22 17:13:18 -0500

    Add Posts Service for data propagation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MeanStackTutorial';
+  // // title = 'MeanStackTutorial';
+  // receivedPosts: Post[] = [];
+
+  // onPostAdded(post: Post) {
+  //   this.receivedPosts.push(post);
+  // }
 }
